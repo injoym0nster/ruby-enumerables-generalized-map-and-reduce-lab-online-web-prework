7bc3c0eba9f7492a1838ca3531f1372b53a440c9
Sahduddin ahmed <53123532+injoym0nster@users.noreply.github.com>
2019-08-10 10:43:06 -0500

    Update and rename lib/my_code.rb to done.rb

---
+++ done.rb
@@ -0,0 +1,17 @@
+def map(array)
+  array.length.times { |index|
+    array[index] = yield(array[index])
+  }
+  array
+end
+
+def reduce(array, starting = 0)
+  array.length.times { |index|
+  if array[index]
+    starting  = yield(array[index], starting)
+  else
+    starting = false
+  end
+  }
+  starting  
+end  Your Code Here
